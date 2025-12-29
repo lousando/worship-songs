51dc6270f327e6e38e4aa809e88188d6e535c95a
Louis Sandoval <louis@sandoval.family>
2025-12-29 15:55:56 -0700

    Dockerize the app

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM oven/bun:1-debian
+
+WORKDIR /app
+
+COPY . .
+
+RUN curl -sfS https://dotenvx.sh | sh
+RUN bun i --frozen-lockfile
+RUN bun run build
+
+CMD dotenvx run -f .env.production -- bun start
--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,31 @@
-import { env } from "cloudflare:workers";
-import { Elysia, type Context } from "elysia";
-import { cors } from '@elysiajs/cors'
-
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { serveStatic } from "hono/bun";
 import { createClient } from "@libsql/client";
 
 export const turso = createClient({
-    url: env.TURSO_DATABASE_URL,
-    authToken: env.TURSO_AUTH_TOKEN,
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-const app = new Elysia({ aot: false, prefix: "/api" })
-    .use(cors())
-    .get("/songs", async (context: Context) => {
-        const { rows } = await turso.execute({
-            sql: "SELECT id,page,name FROM songs ORDER BY page",
-            args: [],
-        });
+const app = new Hono();
 
-        return rows;
-    })
+app
+  .use(cors())
+  .use("/*", serveStatic({ root: "./dist" }))
+  .get("/api/health", (c) => {
+    return c.text("OK");
+  })
+  .get("/api/songs", async (c) => {
+    const { rows } = await turso.execute({
+      sql: "SELECT id,page,name FROM songs ORDER BY page",
+      args: [],
+    });
+
+    return c.json(rows);
+  });
 
 export default {
-    fetch(request: Request) {
-        return app.fetch(request);
-    },
+  port: 8080,
+  fetch: app.fetch,
 };
